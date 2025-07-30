14bb385463e7c8d884c090af3992b71a68f0c2eb
Shatul Patil <coolshatul@gmail.com>
2025-07-30 11:54:49 +0530

    feat: Implement ScrollToTop component and add redirects for index.html

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import Home from './pages/Home';
 import Services from './pages/Services';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-black text-white">
         <Navbar />
         <main>
